8d66491a75b2c296a7a9e12304e02d4d4078e1a6
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-06 12:28:47 +0200

    recursion base solution

--- recursion/fibonacci.py
+++ recursion/fibonacci.py
@@ -1,1 +1,11 @@
-print("solve a recursive problem")
+def fibonacci(n):
+    if n < 0:
+        return (f"Input number should be equal to or above 0:{n}")
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+print(fibonacci(7))
