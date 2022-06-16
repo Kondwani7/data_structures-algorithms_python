ae1cf45b702862bd0f6eeb2a2eb927029ec282ab
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-16 17:36:15 +0200

    print a linkedlist- travseral

--- linked_lists/singly_ll.py
+++ linked_lists/singly_ll.py
@@ -1,16 +1,30 @@
-#singly linked list
 class Node:
-    #constructor
-    def __init__(self, data):
+       def __init__(self, data=None):
         self.data = data
         self.next = None
+
+class SLinkedList:
+   def __init__(self):
+      self.head = None
+
+   def print_LL(self):
+      n = self.head
+      while n is not None:
+            print(n.data)
+            n = n.next
+
+ll1 = SLinkedList()
+ll1.head = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+
+#link nodes
+ll1.head.next = n2
+n2.next = n3
+
+ll1.print_LL()
+
+    
    
-        
-class LinkedList(object):
-    def __init__(self):
-        self.head = None;
 
 
-node1 = Node(10)
- 
-print(node1)
