65d068810c0616bac4bc8708200e8f55da887b7f
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-29 11:55:35 +0200

    array declaration

---
+++ arrays.py
@@ -0,0 +1,8 @@
+import array as arr
+
+a = arr.array('i',[1,2,3])
+for i in range(0, 3):
+    print(a[i], end=' ')
+b = arr.array('d', [3.4, 2.4, 5.44, 0.56])
+for i in range(0, 4):
+    print(b[i], end=" ")
