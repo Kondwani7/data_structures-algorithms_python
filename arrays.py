5128f7fb8ad9958fce042fa1ae31ceae337aca3e
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-06-29 12:22:42 +0200

    split an array and reverse it

--- arrays.py
+++ arrays.py
@@ -26,3 +26,22 @@ a.pop(4)
 for i in range(0,5):
     print(a[i], end=' ')
 print()
+#add 3 back
+a.insert(4,3)
+#print(a)
+#slicing an array, elements in the 4 position and below
+sliced_arr1 = a[:4]
+#elements after the 4th postion
+sliced_arr2 = a[4:]
+for i in range(0,4):
+    print(sliced_arr1[i], end=" ")
+print("first 4 elements")
+#last 2
+for i in range(0,2):
+    print(sliced_arr2[i], end=" ")
+print("last 2 elements")
+#reverse an array
+reversed_arr = a[::-1]
+for i in range(0 ,6):
+    print(reversed_arr[i], end=" ")
+print("reversed array")
