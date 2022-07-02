16c3b21c5306893171b7bfd5363bdb7b24be840b
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-02 16:20:19 +0200

    reverse string stack

--- stacks.py
+++ stacks.py
@@ -1,5 +1,6 @@
 #LIFO
 from collections import deque
+from inspect import stack
 
 
 stack1 = []
@@ -89,3 +90,28 @@ def reverse_rec(s):
     else:
         return reverse(s[1:] + s[0])
 print(reverse_rec("quacks"))
+
+def createStack():
+    stack = []
+    return stack
+def size(stack):
+    return len(stack)
+def isEmpty(stack):
+    if size(stack) == 0:
+        return True
+def push(stack, item):
+    stack.append(item)
+def pop(stack):
+    if isEmpty(stack): return
+    return stack.pop()
+def reverse_stack(string):
+    n = len(string)
+    stack = createStack()
+    for i in range(0, n, 1):
+        push(stack, string[i])
+    string = ""
+    for i in range(0, n ,1 ):
+        string += pop(stack)
+    return string
+
+print(reverse_stack("ddede"))
