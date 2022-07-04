4d3ceff960805ec1299825d84359dd6534f4e5fd
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-04 13:55:21 +0200

    three different ways to implement a queue

---
+++ queues.py
@@ -0,0 +1,42 @@
+# a  list, first in first out FIFO
+#enqueue = add 
+#dequeue = remove
+#queue with list
+from collections import deque
+from queue import Queue
+
+
+queue1 = []
+queue1.append(3)
+queue1.append(5)
+queue1.append(7)
+print(f"queue1 : {queue1}")
+queue1.pop(0)
+print(f"popped queue1: {queue1}")
+#queue with deqeue
+q2 = deque()
+q2.append(2)
+q2.append(5)
+q2.append(8)
+print(f"queue 2: {q2}")
+q2.popleft()
+print(f"popped queue 2: {q2}")
+
+q3 = Queue(maxsize=3)
+print(q3.qsize())
+q3.put(4)
+q3.put(8)
+q3.put(12)
+print(q3.full())
+print("elements in queue 3")
+print(q3.get())
+print(q3.get())
+print(q3.get())
+#empty queue
+print("\nEmpty:",q3.empty())
+q3.put(5)
+q3.put(8)
+print("elements in queue 3")
+print(q3.get())
+print(q3.get())
+print("\nFull", q3.full())
