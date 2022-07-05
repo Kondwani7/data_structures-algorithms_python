04d775aec72d9b721c646969dcccaae338e8b334
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-05 17:09:56 +0200

    preorder traversal

--- binary_trees.py
+++ binary_trees.py
@@ -38,8 +38,15 @@ class Node:
             res.append(root.data)
             res = res + self.inOrderTraversal(root.right)
         return res
-
-
+    #preorder traversal
+    #root -> left -> right
+    def preOrderTraversal(self, root):
+        res = []
+        if root:
+            res.append(root.data)
+            res = res + self.preOrderTraversal(root.left)
+            res = res + self.preOrderTraversal(root.right)
+        return res
 bt1 = Node(21)
 bt1.insert(19)
 bt1.insert(18)
@@ -48,4 +55,6 @@ bt1.insert(23)
 bt1.insert(24)
 bt1.insert(16)
 #bt1.PrintTree()
-print(bt1.inOrderTraversal(bt1))
+print("preorder traversal: ",bt1.inOrderTraversal(bt1))
+#in order traversal
+print("preorder traversal: ", bt1.preOrderTraversal(bt1))
