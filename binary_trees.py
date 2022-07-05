7efdf1aa29af87540996c6da82fbfebd13f98af1
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-05 16:56:09 +0200

    print data in a binary tree

--- binary_trees.py
+++ binary_trees.py
@@ -3,7 +3,7 @@ class Node:
     def __init__(self, data):
         self.left = None
         self.right = None
-        self.data
+        self.data = data
     #insertion
     def insert(self, data):
         #binary tree, if less than curr node go to the left side
@@ -22,3 +22,20 @@ class Node:
         #if the binary tree is empty
         else:
             self.data = data
+    #print data
+    def PrintTree(self):
+        if self.left:
+            self.left.PrintTree()
+        print(self.data)
+        if self.right:
+            self.right.PrintTree()
+
+
+bt1 = Node(21)
+bt1.insert(19)
+bt1.insert(18)
+bt1.insert(22)
+bt1.insert(23)
+bt1.insert(24)
+bt1.insert(16)
+bt1.PrintTree()
