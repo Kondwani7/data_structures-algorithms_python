90c98c6dd0fe6ac4e8cba5b0e1f93fc80e05e6e0
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-11 19:36:33 +0200

    inserting in a trie

--- trie.py
+++ trie.py
@@ -5,3 +5,16 @@ class Trie:
     def __init__(self, is_end=False):
         self.children = {}
         self.is_end = is_end
+    #inserting in a trie
+    def inser(self, s):
+        node = self
+        #for a character in a string
+        for ch in s:
+            if ch not in node.children:
+                #insert ch
+                node.children[ch] = Trie()
+            #if not create it in children
+            node = node.children[ch]
+            #end string
+        node.is_end= True
+    
