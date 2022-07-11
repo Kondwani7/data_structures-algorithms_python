5a893f488f59ce00d8e1038d8be7938c6d1a5d70
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-11 15:32:48 +0200

    priority queue

--- heap.py
+++ heap.py
@@ -191,5 +191,23 @@ heapsort(arr1)
 
 #priority queue a max heap
 class PriorityQueue:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.queue = MaxHeap()
+    #add an element
+    def enqueue(self, element):
+        self.queue.insert(element)
+    #remove an element FIFO
+    def dequeue(self, element):
+        return self.queue.extract_max()
+    #peek - top of queue
+    def peek(self):
+        return self.queue.get_max()
+    #check if empty
+    def is_empty(self):
+        return len(self.queue.heap) == 0
+    #change priority by inex (update)
+    def change_priority_by_index(self, i, new):
+        self.queue.update_by_index(i, new)
+    #change priority from a old value to new
+    def change_priority(self, old , new):
+        self.queue.update(old, new)
