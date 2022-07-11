3bab8aad99018e709820a84f1dd3704a2a94e099
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-11 13:08:31 +0200

    sift down

--- heap.py
+++ heap.py
@@ -30,4 +30,17 @@ class MinHeap:
         it sift dow (swap) values larger than the ones below it
         until a heap structure is created
         """
-        pass
+        #left and right of heap
+        left = 2*i + 1
+        right = 2*i + 2
+        #while left or right is smaller than the len of heap and value i is greater than left or right
+        while(left < len(self.heap) and self.heap[i] > self.heap[left]) or (right < len(self.heap) and self.heap[i] > self.heap[right]):
+            #check for the smallest nodes between left or right that will be swapped with i
+            smallest = left if (right >= len(self.heap) or self.heap[left] < self.heap[right]) else right
+            #swap smallest with i
+            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+            #until i is the smallest
+            i = smallest
+            #reset left and right
+            left = 2*i + 1
+            right = 2*i + 2
