f46760cdf656272bbecf6e0dce024b255a29ea77
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-13 10:03:55 +0200

    graph adjaceny list

--- graphs.py
+++ graphs.py
@@ -10,7 +10,7 @@ from unicodedata import name
 class VertexM:
     def __init__(self, n):
         self.node = n
-
+#O(n^2)
 class GraphM:
     vertices = {}
     edges = []
@@ -54,5 +54,48 @@ for i in range(ord('A'), ord('K')):
 edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
 for edge in edges:
     g.add_edge(edge[:1], edge[1:])
-
+print("graph adjaceny matrix")
 g.print_graph()
+
+#graph adjacey list
+class Vertex:
+    def __init__(self, node):
+        self.node = node
+        self.neighbors = list()
+    
+    def add_neighbor(self, v, weight):
+        if v not in self.neighbors:
+            self.neighbors.append((v, weight))
+            self.neighbors.sort()
+#list graph - o(logn)
+class Graph:
+    vertices = {}
+    def add_vertex(self, vertex):
+        if isinstance(vertex, Vertex) and vertex.node not in self.vertices:
+            self.vertices[vertex.node] = vertex
+            return True
+        else:
+            return False
+    def add_edge(self, u, v, weight=0):
+        if u in self.vertices and v in self.vertices:
+            self.vertices[u].add_neighbor(v, weight)
+            self.vertices[v].add_neighbor(u, weight)
+            return True
+        else:
+            return False
+    def print_graph(self):
+        for key in sorted(list(self.vertices.keys())):
+            print(key + str(self.vertices[key].neighbors))
+    
+g2 = Graph()
+a = Vertex('A')
+g2.add_vertex(a)
+g2.add_vertex(Vertex('B'))
+for i in range(ord('A'), ord('K')):
+    g2.add_vertex(Vertex(chr(i)))
+
+
+for edge in edges:
+    g2.add_edge(edge[:1], edge[1:])
+print("graph adjaceny list")
+g2.print_graph()
