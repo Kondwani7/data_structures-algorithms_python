ecbb9ccdfd7213e08faeda0174d39c6542b6bab0
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-13 11:51:30 +0200

    toplogical sort with DFS

--- graph_applications.py
+++ graph_applications.py
@@ -6,8 +6,10 @@ from collections import defaultdict
 
 
 class Graph:
-    def __init__(self):
+    def __init__(self, vertices):
         self.graph = defaultdict(list)
+        #vertices
+        self.V = vertices
     #add an edge
     def add_edge(self, u, v):
         self.graph[u].append(v)
@@ -49,9 +51,31 @@ class Graph:
                     queue.append(i)
                     #list as visited now
                     visited[i] = True
+    #topological sorting into DAGS -> applications  scheduling jbos: Airflow ETL or AI pipelines
+    #topological sorting with DFS fist
+    def topologicalSortUtil(self, v, visited, stack):
+        visited[v] = True
+        #visited all node v in graph 
+        for i in self.graph[v]:
+            #if not vistied
+            if visited[i] == False:
+                #recusively visit
+                self.topologicalSortUtil(i, visited, stack)
+        #then append to stack
+        stack.append(v)
+    #complete topological sort function
+    def topologicalSort(self):
+        #mark all vertices not visited
+        visited = [False] *self.V
+        stack = []
+        for i in range(self.V):
+            if visited[i] == False:
+                self.topologicalSortUtil(i, visited, stack)
+        #print stack
+        print(stack[::-1])
 
 print("DFS recursive")
-g1 = Graph()
+g1 = Graph(4)
 g1.add_edge(0, 1)
 g1.add_edge(0, 2)
 g1.add_edge(1, 2)
@@ -61,3 +85,13 @@ g1.add_edge(3,3)
 g1.DFS()
 print("BFS")
 g1.BFS(0)
+g2 = Graph(6)
+print("topological sort")
+g2.add_edge(5, 2)
+g2.add_edge(5, 0)
+g2.add_edge(4,0)
+g2.add_edge(4,1)
+g2.add_edge(2,3)
+g2.add_edge(3,1)
+g1.topologicalSort()
+g2.topologicalSort()
