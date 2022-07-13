d7a003d0bb367f12c43b37137f9553ef2c303c77
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-13 10:47:51 +0200

    Breadth first search graph

--- graph_applications.py
+++ graph_applications.py
@@ -24,6 +24,31 @@ class Graph:
         for vertex in self.graph:
             if vertex not in visited:
                 self.DFSUtil(vertex, visited)
+    #breadth first seach - level order traversal
+    def BFS(self, s):
+        """
+        this function traverses a graph with BFS
+        Args:
+            s - the starting point of traversal
+        """
+        #mark all vertices not visited
+        visited = [False] * (max(self.graph) + 1)
+        #use queues FIFO
+        queue = []
+        queue.append(s)
+        visited[s] = True
+        #while the queue exists
+        while queue:
+            #removed element from queue -dequeue
+            s = queue.pop()
+            print(s, end=" ")
+            #for all vertices dequeued s,
+            for i in self.graph[s]:
+                #if not visited mark it
+                if visited[i] == False:
+                    queue.append(i)
+                    #list as visited now
+                    visited[i] = True
 
 print("DFS recursive")
 g1 = Graph()
@@ -34,3 +59,5 @@ g1.add_edge(2, 0)
 g1.add_edge(2, 3)
 g1.add_edge(3,3)
 g1.DFS()
+print("BFS")
+g1.BFS(0)
