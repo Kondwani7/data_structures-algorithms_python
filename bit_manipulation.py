3e6f073ff4ffdddae76e3ad5836ed9b268f76b7e
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-14 11:46:35 +0200

    base 10

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -25,3 +25,7 @@ N = 21
 print("The bit at 3rd position :", 1 if (getBit(N, 3)) else 0 )
 print("the value of our number changes after setting the bit to:", setBit(N, 0))
 print("The value of our number changes after clearing the bit to:", clearBit(N, 0))
+#base 2 for example
+#in decimal 1234  in base 10 is 
+print("1234 in base 10:", (1 * 10**3 + 2 * 10**2 + 3 * 10 **1 + 4 * 10 ** 0 ))
+
