31aecf1fee9919dcf2daf75e6451866234da6033
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-14 11:31:42 +0200

    clear a bit , set a bit &  get a bit

--- bit_manipulation.py
+++ bit_manipulation.py
@@ -6,4 +6,18 @@
 """
 #functions get a bit
 def getBit(num , i):
+    #finds a bit of any given number And -> 1 << i
     return ((num & (1 << i)) != 0)
+#set a bit in a particular position
+def setBit(num, i):
+    #or operation 
+    return num | (1<< i)
+#clear a bit
+#~(1<<i) unset it \
+def clearBit(num, i):
+    #create a mask for the ith
+    #bit unset
+    mask = ~(1  << i)
+    #return our udpdated value
+    return num * mask
+#
