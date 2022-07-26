40ef21195b460079e9aa50e43f784e33c8cf0623
Kondwani7 <kondwanigideon.ngulube@gmail.com>
2022-07-26 09:03:36 +0200

    prefix product

--- prefixs.py
+++ prefixs.py
@@ -11,10 +11,25 @@ def prefixSum(arr):
     for i in range(1,len(arr)):
         #our answer is the answer at the value before it + ith position of value in array
         ans[i] = ans[i-1] + arr[i]
-    return print(ans)
+    return print("prefix sum array:",ans)
 arr1 = [3, 2, 5, 43, 2]
 arr2 = [1, 2, 3, 4,5,6]
 print("inital array:", arr1)
 prefixSum(arr1)
 print("inital array:", arr2)
 prefixSum(arr2)
+#prefix product
+def prefixProduct(arr):
+    def prod(arr):
+        ans = 1
+        for i in arr:
+            ans = ans * i
+        return ans
+    #get the product starting form the second value arr[1] through our whole array
+    ans = [prod(arr[:i+1]) for i in range(len(arr))]
+    return print("prefix product", ans)
+
+print("inital array:", arr1)
+prefixProduct(arr1)
+print("inital array:", arr2)
+prefixProduct(arr2)
